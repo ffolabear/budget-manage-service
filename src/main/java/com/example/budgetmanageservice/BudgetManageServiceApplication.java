31916a473f957d1662ed1495b961e84943030492
ffolabear <ffolabear@gmail.com>
2023-11-10 22:56:05 +0900

    init : 프로젝트 초기설정

---
+++ src/main/java/com/example/budgetmanageservice/BudgetManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.budgetmanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManageServiceApplication.class, args);
+    }
+
+}
